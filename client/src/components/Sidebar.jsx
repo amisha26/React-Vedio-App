66402d48466aa56b50db665e3d91745d75520c12
amisha26 <amisha8299@gmail.com>
2021-10-27 23:25:59 +0530

    comp

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,6 +6,37 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import { SocketContext } from '../Context';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  gridContainer: {
+    width: '100%',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  container: {
+    width: '600px',
+    margin: '35px 0',
+    padding: 0,
+    [theme.breakpoints.down('xs')]: {
+      width: '80%',
+    },
+  },
+  margin: {
+    marginTop: 20,
+  },
+  padding: {
+    padding: 20,
+  },
+  paper: {
+    padding: '10px 20px',
+    border: '2px solid black',
+  },
+}));
+
 const Sidebar = ({ children }) => {
   const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } = useContext(SocketContext);
   const [idToCall, setIdToCall] = useState('');
