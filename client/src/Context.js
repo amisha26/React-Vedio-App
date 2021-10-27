6bc0de8f685b3b6e2a0c48b6b6c530391074d212
amisha26 <amisha8299@gmail.com>
2021-10-27 23:28:11 +0530

    context

---
+++ client/src/Context.js
@@ -0,0 +1,68 @@
+import React, { createContext, useState, useRef, useEffect } from 'react';
+import { io } from 'socket.io-client';
+import Peer from 'simple-peer';
+
+const SocketContext = createContext();
+
+// const socket = io('http://localhost:5000');
+const socket = io('https://chat-att.herokuapp.com/');
+
+const ContextProvider = ({ children }) => {
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
+  const [stream, setStream] = useState();
+  const [name, setName] = useState('');
+  const [call, setCall] = useState({});
+  const [me, setMe] = useState('');
+
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then((currentStream) => {
+        setStream(currentStream);
+
+        myVideo.current.srcObject = currentStream;
+      });
+
+    socket.on('me', (id) => setMe(id));
+
+    socket.on('callUser', ({ from, name: callerName, signal }) => {
+      setCall({ isReceivingCall: true, from, name: callerName, signal });
+    });
+  }, []);
+
+  
+
+  const leaveCall = () => {
+    setCallEnded(true);
+
+    connectionRef.current.destroy();
+
+    window.location.reload();
+  };
+
+  return (
+    <SocketContext.Provider value={{
+      call,
+      callAccepted,
+      myVideo,
+      userVideo,
+      stream,
+      name,
+      setName,
+      callEnded,
+      me,
+      callUser,
+      leaveCall,
+      answerCall,
+    }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export { ContextProvider, SocketContext };
